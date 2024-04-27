895ac7d2bdbb9bcd4ba51a541de401fc61b6e63b
pclank <31415965+pclank@users.noreply.github.com>
2024-04-27 23:40:35 +0200

    added inputs to camera.

--- app/src/main/java/com/example/raytracer/Camera.kt
+++ app/src/main/java/com/example/raytracer/Camera.kt
@@ -1,5 +1,11 @@
+package com.example.raytracer
+
 import Ray
 import float3
+
+enum class InputType {
+    FWD, AFT, MOVE_LEFT, MOVE_RIGHT, UP, DOWN, ROT_LEFT, ROT_RIGHT
+}
 
 class Camera(var camPos: float3, var camDir: float3, val SCRWIDTH: Int, val SCRHEIGHT: Int) {
     val aspect: Float = SCRWIDTH.toFloat() / SCRHEIGHT.toFloat()
@@ -17,5 +23,39 @@ class Camera(var camPos: float3, var camDir: float3, val SCRWIDTH: Int, val SCRH
         val p_c = (P - camPos)
         p_c.normalize()
         return Ray(camPos, p_c)
+    }
+
+    fun InputHandle(but: InputType, t: Float)
+    {
+        val speed = 0.0025f * t
+        var ahead = camDir - camPos
+        ahead.normalize()
+        var tmpUp = float3(0.0f, 1.0f, 0.0f);
+        var right = tmpUp.cross(ahead)
+        right.normalize()
+        var up = ahead.cross(right)
+        up.normalize()
+
+        if (but == InputType.MOVE_LEFT) camPos -= right * speed * 2.0f
+        if (but == InputType.MOVE_RIGHT) camPos += right * speed * 2.0f
+        if (but == InputType.FWD) camPos += ahead * speed * 2.0f
+        if (but == InputType.AFT) camPos -= ahead * speed * 2.0f
+
+        camDir = camPos + ahead;
+
+        if (but == InputType.UP) camDir -= up * speed
+        if (but == InputType.DOWN) camDir += up * speed
+        if (but == InputType.ROT_LEFT) camDir -= right * speed
+        if (but == InputType.ROT_RIGHT) camDir += right * speed
+
+        ahead = camDir - camPos
+        ahead.normalize()
+        up = ahead.cross(right)
+        up.normalize()
+        right = up.cross(ahead)
+        right.normalize()
+        topLeft = camPos + ahead * 2.0f - right * aspect + up;
+        topRight = camPos + ahead * 2.0f + right * aspect + up;
+        bottomLeft = camPos + ahead * 2.0f - right * aspect - up;
     }
 }
--- app/src/main/java/com/example/raytracer/MainActivity.kt
+++ app/src/main/java/com/example/raytracer/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.raytracer
 
-import Camera
 import Sphere
 import android.content.Context
 import android.graphics.Bitmap
@@ -14,16 +13,40 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Snackbar
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.graphics.createBitmap
 import com.example.raytracer.ui.theme.RayTracerTheme
 import float3
+import kotlinx.coroutines.delay
+import java.util.Timer
+import java.util.TimerTask
+
+// Constants
+//    val SCRWIDTH = 256
+//    val SCRHEIGHT = 256
+val SCRWIDTH = 512
+val SCRHEIGHT = 512
+
+// Create camera
+val cam = Camera(float3(0.0f, 0.0f, -2.0f), float3(0.0f, 0.0f, -1.0f), SCRWIDTH, SCRHEIGHT)
+
+var bmap: ImageBitmap = ImageBitmap(SCRWIDTH, SCRHEIGHT)
+
+val t: Timer = Timer()
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,8 +58,55 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+//                    Greeting("Android")
+                    t.scheduleAtFixedRate(
+                        object : TimerTask() {
+                            override fun run() {
+                                UpdatePixels()
+                            }},
+                        0, 1000
+                    )
                     PixelColorTest(baseContext)
+                }
+                Surface(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(0.dp),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Button(onClick = { cam.InputHandle(InputType.FWD, 0.001f) }) {
+                        Text(text = "Fwd")
+                    }
+                }
+                Surface(
+                    modifier = Modifier
+                        .width(250.dp)
+                        .padding(75.dp),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Button(onClick = { cam.InputHandle(InputType.MOVE_LEFT, 0.001f) }) {
+                        Text(text = "Left")
+                    }
+                }
+                Surface(
+                    modifier = Modifier
+                        .width(250.dp)
+                        .padding(75.dp),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Button(onClick = { cam.InputHandle(InputType.MOVE_RIGHT, 0.001f) }) {
+                        Text(text = "Right")
+                    }
+                }
+                Surface(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(125.dp),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Button(onClick = { cam.InputHandle(InputType.AFT, 0.001f) }) {
+                        Text(text = "Aft")
+                    }
                 }
             }
         }
@@ -69,19 +139,13 @@ class MyDrawable : Drawable() {
         PixelFormat.OPAQUE
 }
 
-@Composable
-fun PixelColorTest(con: Context)
+fun UpdatePixels()
 {
-    val SCRWIDTH = 256
-    val SCRHEIGHT = 256
-
-    // Create camera
-    val cam = Camera(float3(0.0f, 0.0f, -2.0f), float3(0.0f, 0.0f, -1.0f), SCRWIDTH, SCRHEIGHT)
-
     // Add sphere
     val sp0 = Sphere(1.0f, float3(0.0f, 0.0f, 0.0f), 0)
 
-    val cols: IntArray = IntArray(SCRWIDTH*SCRHEIGHT) {1 * it}
+//    val cols: IntArray = IntArray(SCRWIDTH*SCRHEIGHT) {1 * it}
+    val cols: IntArray = IntArray(SCRWIDTH*SCRHEIGHT)
 
     // Trace rays
     for (i in 0 until SCRWIDTH)
@@ -106,7 +170,42 @@ fun PixelColorTest(con: Context)
 
 //    bitmap.setPixels(cols, 0, 256, 0, 0, SCRWIDTH, SCRHEIGHT)
     bitmap.setPixels(cols, 0, SCRWIDTH, 0, 0, SCRWIDTH, SCRHEIGHT)
-    val bmap = bitmap.asImageBitmap()
+    bmap = bitmap.asImageBitmap()
+}
+
+@Composable
+fun PixelColorTest(con: Context)
+{
+    // Add sphere
+    val sp0 = Sphere(1.0f, float3(0.0f, 0.0f, 0.0f), 0)
+
+//    val cols: IntArray = IntArray(SCRWIDTH*SCRHEIGHT) {1 * it}
+    val cols: IntArray = IntArray(SCRWIDTH*SCRHEIGHT)
+
+    // Trace rays
+    for (i in 0 until SCRWIDTH)
+        for (j in 0 until SCRHEIGHT)
+        {
+            val ray = cam.GeneratePrimaryRay(i.toFloat(), j.toFloat())
+
+            // Intersect sphere
+            sp0.intersect(ray)
+
+            // Check for hit
+            if (ray.t < 1e30f)
+            {
+                ray.objIdx = sp0.id
+                cols[i + j * SCRWIDTH] = 255
+            }
+            else
+                cols[i + j * SCRWIDTH] = 0
+        }
+
+    var bitmap = createBitmap(width=SCRWIDTH, height=SCRHEIGHT, config=Bitmap.Config.RGB_565)
+
+//    bitmap.setPixels(cols, 0, 256, 0, 0, SCRWIDTH, SCRHEIGHT)
+    bitmap.setPixels(cols, 0, SCRWIDTH, 0, 0, SCRWIDTH, SCRHEIGHT)
+    bmap = bitmap.asImageBitmap()
     Image(bitmap = bmap, "yes")
     //image.contentDescription = resources.getString(R.string.my_image_desc)
 }
